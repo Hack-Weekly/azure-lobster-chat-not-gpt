9c0a9df20e391ff5b1b2516614d68165406c92cc
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-03-20 19:23:57 +0000

    pit stop

--- app/src/routes/components/chat-interface/chatHeader.jsx
+++ app/src/routes/components/chat-interface/chatHeader.jsx
@@ -1,9 +1,12 @@
+import React, { Component } from 'react'
+import NavImg from "src/img/nav-img.png"
+
 export default function ChatHeader() {
   return (
     <>
       <header>
         <div className="nav">
-          <img className="user-img" src=""></img>
+          <img className="user-img" src={NavImg}></img>
           <span className="user-name">Username</span>
         </div>
       </header>
