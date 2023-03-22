595fa290ec2c2eec8ab9321183836428b6b2a14d
Cihaan <cihankafadar1@gmail.com>
2023-03-22 23:16:34 +0100

    feat: create/leave room

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import { Application, Request, Response } from "express";
 import { createServer } from "http";
-import { Socket } from "socket.io";
-const { Server } = require("socket.io");
+import { Server, Socket } from "socket.io";
+const { Server: Serve } = require("socket.io");
 
 const EXPRESS_PORT = 5000;
 const SOCKET_PORT = 5500;
@@ -13,7 +13,7 @@ const app: Application = express();
 app.use(cors());
 
 const http = createServer(app);
-const io = new Server(http, {
+const io: Server = new Serve(http, {
   cors: {
     origin: "*", // change when in dev or prod server
     methods: ["GET", "POST", "PUT", "DELETE"],
@@ -25,17 +25,50 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 io.on("connection", (socket: Socket) => {
-  console.log("a user connected");
+  logToServer(`user ${socket.id} is connected to the socket`);
+
+  socket.on("create_room", (room: string) => {
+    socket.join(room);
+
+    logToServer(`user ${socket.id} has created room ${room}`);
+    emitToClient(
+      "create_room",
+      room,
+      `user ${socket.id} has created room ${room}!`
+    );
+    emitToClient(
+      "new_connection",
+      room,
+      `${socket.id} has joined the room ${room}!`
+    );
+
+    logToServer(`user ${socket.id} has joined room ${room}`);
+  });
+
+  socket.on("leave_room", (room: string) => {
+    emitToClient("leave_room", room, `${socket.id} has left the room ${room}!`);
+    socket.leave(room);
+
+    logToServer(`user ${socket.id} has left the room ${room}`);
+  });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    logToServer(`user ${socket.id} disconnected`);
   });
 });
 
+function logToServer(message: string) {
+  console.log(message);
+}
+
+function emitToClient(event: string, room: string, message: string) {
+  io.to(room).emit(event, message);
+}
+
 http.listen(SOCKET_PORT, () => {
-  console.log(`server started on port ${SOCKET_PORT}`);
+  console.log(`Websocket is listenning on port ${SOCKET_PORT}`);
 });
 
 app.listen(EXPRESS_PORT, () => {
-  console.log(`server started on port ${EXPRESS_PORT}`);
+  console.log(`Express server started on port ${EXPRESS_PORT}`);
 });
