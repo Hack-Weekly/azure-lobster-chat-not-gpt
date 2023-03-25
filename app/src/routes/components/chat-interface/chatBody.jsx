3fd37be019b51c1773c4ab79fa3cf18e0f83d924
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-03-25 15:00:16 +0000

    update chatbody.jsx

--- app/src/routes/components/chat-interface/chatBody.jsx
+++ app/src/routes/components/chat-interface/chatBody.jsx
@@ -1,7 +1,10 @@
+import NavImg from "src/img/nav-img.png"
+
 export default function ChatBody() {
   return (
     <>
       <div class="chat-body">
+        <img className="user-img" src={NavImg}></img>
       </div>
     </>
   );
